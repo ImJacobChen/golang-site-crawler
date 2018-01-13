00f85c90d7421e71f1ae9ecb088a33c0e59845eb
JacobChen1994 <jacobchen17@googlemail.com>
2018-01-13 14:40:45 +0000

    Implementing goroutines to crawl multiple pages

--- main.go
+++ main.go
@@ -4,8 +4,54 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
+
 	"golang.org/x/net/html"
 )
+
+var links = []string{}
+var wg sync.WaitGroup
+
+func crawlPage(url string) {
+	fmt.Println("Crawling: ", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	z := html.NewTokenizer(resp.Body)
+
+	for {
+		// Next token
+		tt := z.Next()
+
+		if tt == html.ErrorToken {
+			return
+		} else if tt == html.StartTagToken {
+			t := z.Token()
+
+			isAnchor := t.Data == "a"
+			if isAnchor {
+				// Loop over anchor attributes and
+				// if there is a 'href' then Print
+				// the value.
+				for _, a := range t.Attr {
+					if a.Key == "href" {
+						fmt.Println("Found link!: ", a.Val)
+						links = append(links, a.Val)
+						wg.Add(1)
+						go crawlPage(a.Val)
+						break
+					}
+				}
+			}
+		}
+	}
+
+	wg.Done()
+}
 
 func main() {
 
@@ -18,13 +64,31 @@ func main() {
 	z := html.NewTokenizer(resp.Body)
 
 	for {
-		tt := z.Next();
+		// Next token
+		tt := z.Next()
+
 		if tt == html.ErrorToken {
 			return
 		} else if tt == html.StartTagToken {
 			t := z.Token()
-			fmt.Println(t.String()+"\n")
+
+			isAnchor := t.Data == "a"
+			if isAnchor {
+				// Loop over anchor attributes and
+				// if there is a 'href' then Print
+				// the value.
+				for _, a := range t.Attr {
+					if a.Key == "href" {
+						fmt.Println("Found link!: ", a.Val)
+						links = append(links, a.Val)
+						wg.Add(1)
+						go crawlPage(a.Val)
+						break
+					}
+				}
+			}
 		}
 	}
 
+	wg.Wait()
 }
