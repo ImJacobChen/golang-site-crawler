80de075a7c6fa8ab39a586cf658f6aa3ba61a3b2
JacobChen1994 <jacobchen17@googlemail.com>
2018-01-16 20:06:57 +0000

    Implement rudimentary logic to crawl only the chosen domain and not crawl already crawled pages.

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
+	"strings"
 	"sync"
 
 	"golang.org/x/net/html"
 )
 
 var links = []string{}
+var crawledPages = []string{}
 var wg sync.WaitGroup
+var startURL *url.URL
 
 func crawlPage(url string) {
 	fmt.Println("Crawling: ", url)
+	crawledPages = append(crawledPages, url)
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
@@ -30,39 +36,40 @@ func crawlPage(url string) {
 			break
 		}
 
-		// if tt == html.StartTagToken {
-		// 	t := z.Token()
-
-		// 	isAnchor := t.Data == "a"
-		// 	if isAnchor {
-		// 		// Loop over anchor attributes and
-		// 		// if there is a 'href' then Print
-		// 		// the value.
-		// 		for _, a := range t.Attr {
-		// 			if a.Key == "href" {
-		// 				fmt.Println("Found link!: ", a.Val)
-		// 				fmt.Println(z.Text())
-		// 				links = append(links, a.Val)
-		// 				wg.Add(1)
-		// 				go crawlPage(a.Val)
-		// 				break
-		// 			}
-		// 		}
-		// 	}
-		// }
-
 		if tt == html.StartTagToken {
 			t := z.Token()
-			if t.Data == "title" {
-				tokenType := z.Next()
-				if tokenType == html.TextToken {
-					titleToken := z.Token()
-					fmt.Println("Found title: ", titleToken.String())
+
+			isAnchor := t.Data == "a"
+			if isAnchor {
+				// Loop over anchor attributes and
+				// if there is a 'href' then Print
+				// the value.
+				for _, a := range t.Attr {
+					if a.Key == "href" {
+						//fmt.Println("Found link!: ", a.Val)
+						links = append(links, a.Val)
+						if pageShouldBeCrawled(a.Val) {
+							wg.Add(1)
+							go crawlPage(a.Val)
+						}
+						break
+					}
 				}
-
 			}
-
 		}
+
+		// if tt == html.StartTagToken {
+		// 	t := z.Token()
+		// 	if t.Data == "title" {
+		// 		tokenType := z.Next()
+		// 		if tokenType == html.TextToken {
+		// 			titleToken := z.Token()
+		// 			fmt.Println("Found title: ", titleToken.String())
+		// 		}
+
+		// 	}
+
+		// }
 
 		// if tt == html.TextToken {
 		// 	token := z.Token()
@@ -71,12 +78,35 @@ func crawlPage(url string) {
 		// }
 	}
 
-	//wg.Done()
+	wg.Done()
+}
+
+func pageShouldBeCrawled(url string) bool {
+
+	crawledPagesLen := len(crawledPages)
+	for i := 0; i < crawledPagesLen; i++ {
+		if url == crawledPages[i] {
+			return false
+		}
+	}
+
+	if !strings.Contains(url, startURL.Hostname()) {
+		return false
+	}
+
+	return true
 }
 
 func main() {
 
-	crawlPage("https://www.fusion-conferences.com")
+	url, err := url.Parse("https://www.fusion-conferences.com")
+	if err != nil {
+		log.Fatalln("Invalid start URL")
+	}
 
-	//wg.Wait()
+	startURL = url
+	wg.Add(1)
+	crawlPage(startURL.String())
+
+	wg.Wait()
 }
